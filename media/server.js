0e0d440176c6f59be76bb8e85f7a4f80bed91903
inventionpro <109528211+inventionpro@users.noreply.github.com>
2026-06-27 21:00:19 +0200

    Server/Login redesign | Few improvements

--- media/server.js
+++ media/server.js
@@ -30,6 +30,7 @@ const ServerInput = document.getElementById('server');
 ServerInput.onchange = ServerInput.oninput = function(){
   if (curServerTime) clearTimeout(curServerTime);
   ServerInput.setAttribute('invalid', true);
+  if (ServerInput.value==='') return;
   curServerTime = setTimeout(()=>{
     checkServer(ServerInput.value)
       .then(valid=>{
@@ -44,13 +45,13 @@ function showServerList() {
     .map(srv=>`<span data-id="${srv.id}" data-url="${encodeURIComponent(srv.url)}"${onlineServers[srv.url]?' online':''}${document.querySelector('#server-list > span[selected]')?.getAttribute('data-id')===srv.id?' selected':''}>
   <button${document.querySelector(`#server-list > span[data-url="${encodeURIComponent(srv.url)}"][selected]`)?' selected':''}>
     <span>${sanitizeHTML(srv.url)}</span>
-    <span>
-      ${srv.name?`<span style="font-size:90%"><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 256 256"><path fill-rule="evenodd" clip-rule="evenodd" d="M128 128C163.346 128 192 99.3462 192 64C192 28.6538 163.346 0 128 0C92.6538 0 64 28.6538 64 64C64 99.3462 92.6538 128 128 128ZM151 146H148H108H105C49.7715 146 5 190.772 5 246V256H108H148H251V246C251 190.772 206.228 146 151 146Z"/></svg>${sanitizeMinimChars(srv.name)}</span>`:''}
-      ${extraServers[srv.url]?.dev?'<span title="This is a dev server">⚠️</span>':''}
-      ${extraServers[srv.url]?.vermiss?'<span title="There is a version missmatch">❌</span>':''}
+    <span style="font-size:90%" class="extra">
+      ${srv.name?`<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 256 256"><path fill-rule="evenodd" clip-rule="evenodd" d="M128 128C163.346 128 192 99.3462 192 64C192 28.6538 163.346 0 128 0C92.6538 0 64 28.6538 64 64C64 99.3462 92.6538 128 128 128ZM151 146H148H108H105C49.7715 146 5 190.772 5 246V256H108H148H251V246C251 190.772 206.228 146 151 146Z"/></svg>${sanitizeMinimChars(srv.name)}`:''}
+      ${extraServers[srv.url]?.dev?'<span tlang="servers.dev">⚠️ Dev server</span>':''}
+      ${extraServers[srv.url]?.vermiss?'<span tlang="servers.mismatch">❌ Version mismatch</span>':''}
     </span>
   </button>
-  <button class="del">x</button>
+  <button class="del"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 256 256"><path d="M42.6776 7.32227C32.9145 -2.44063 17.0852 -2.44077 7.32214 7.32227C-2.44082 17.0853 -2.44069 32.9146 7.32214 42.6777L92.2616 127.617L7.32214 212.557C-2.44091 222.32 -2.44083 238.149 7.32214 247.912C17.0852 257.675 32.9145 257.675 42.6776 247.912L127.617 162.973L212.557 247.912C222.32 257.675 238.149 257.675 247.912 247.912C257.675 238.149 257.675 222.32 247.912 212.557L162.973 127.617L247.912 42.6777C257.675 32.9146 257.675 17.0853 247.912 7.32227C238.149 -2.44079 222.32 -2.44068 212.557 7.32227L127.617 92.2617L42.6776 7.32227Z"/></svg></button>
 </span>`)
     .join('');
   document.querySelectorAll('#server-list > span').forEach(spn=>{
@@ -150,8 +151,14 @@ checkOnlineInter = setInterval(()=>{
     if (onlineServers[srv.url]===undefined) {
       con = true;
       checkServer(srv.url)
-        .then(()=>{
-          showServerList();
+        .then(online=>{
+          document.querySelectorAll('#server-list span[data-url="'+encodeURIComponent(srv.url)+'"]')
+            .forEach(entry=>{
+              if (online) entry.setAttribute('online','');
+              if (extraServers[srv.url]?.dev) entry.querySelector('.extra').insertAdjacentHTML('beforeend','<span tlang="servers.dev">⚠️ Dev server</span>');
+              if (extraServers[srv.url]?.vermiss) entry.querySelector('.extra').insertAdjacentHTML('beforeend','<span tlang="servers.mismatch">❌ Version mismatch</span>');
+              window.translate();
+            });
         });
       return;
     }
